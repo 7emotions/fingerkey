37dee58b25495be883c00780888b912208a44c60
Lorenzo Feng <paradise_c@qq.com>
2026-09-09 01:10:05 +0800

    feat(app): native Bluetooth SPP platform channel

--- app/android/app/src/main/kotlin/com/phonefprint/auth/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/phonefprint/auth/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.phonefprint.auth
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Registers the native Bluetooth SPP MethodChannel and EventChannel.
+        flutterEngine.plugins.add(BtSppChannel())
+    }
+}
